1045aa318149c94983ec277d6c0ea3e0630896fb
maan81 <pranijman@gmail.com>
2014-05-05 23:55:33 +0545

    homepage design

---
+++ index.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=0" />
+	<title>Amrit-Prithvi Trust</title>
+	<link rel="shortcut icon" type="image/x-icon" href="css/images/favicon.ico" />
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+	<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="all" />
+	<link href='http://fonts.googleapis.com/css?family=Ubuntu:400,500,700' rel='stylesheet' type='text/css' />
+	
+	<script src="js/jquery-1.8.0.min.js" type="text/javascript"></script>
+	<!--[if lt IE 9]>
+		<script src="js/modernizr.custom.js"></script>
+	<![endif]-->
+	<script src="js/jquery.flexslider-min.js" type="text/javascript"></script>
+	<script src="js/functions.js" type="text/javascript"></script>
+</head>
+<body>
+	<div id="wrapper">
+		
+		<!-- top-nav -->
+		<nav class="top-nav">
+			<div class="shell">
+				<a href="#" class="nav-btn">HOMEPAGE<span></span></a>
+				<span class="top-nav-shadow"></span>
+				<ul>
+					<li class="active"><span><a href="#">Home</a></span></li>
+					<li><span><a href="#">Trust Funds</a></span></li>
+					<li><span><a href="#">Activities</a></span></li>
+					<li><span><a href="#">Our Members</a></span></li>
+					<li><span><a href="#">Contact Us</a></span></li>
+					<li><span><a href="#">Gallery</a></span></li>
+				</ul>
+			</div>
+		</nav>
+		<!-- end of top-nav -->
+
+		<!-- header -->
+		<header  id="header">
+			<!-- shell -->
+			<div class="shell">
+				<div class="header-inner">
+					
+					<!-- header-cnt -->
+					<div class="header-cnt">
+						<h1 id="logo">
+							<a href="#">Amrit-Prithvi Trust</a>
+						</h1>
+						<h1 id="logo2">
+							<a href="#">अमृत-पृथ्वी अविनाशि गुठी</a>
+						</h1>
+
+						<p>
+							<span class="mobile">Education is the main key to development </span>
+						</p>
+						<a href="#" class="blue-btn">LEARN MORE</a>
+					</div>
+					<!-- end of header-cnt -->
+					
+					<!-- slider -->
+					<div class="slider-holder">
+						<div class="flexslider">
+							<ul class="slides">
+								<li><img src="css/images/slide-img.png" alt="" /></li>
+							
+								<li><img src="css/images/slide-img.png" alt="" /></li>
+
+								<li><img src="css/images/slide-img.png" alt="" /></li>
+
+								<li><img src="css/images/slide-img.png" alt="" /></li>
+
+								<li><img src="css/images/slide-img.png" alt="" /></li>
+							</ul>
+						</div>
+					</div>
+					<!-- end of slider -->
+					<div class="cl">&nbsp;</div>
+				</div>
+				<div class="cl">&nbsp;</div>
+			</div>
+			<!-- end of shell -->	
+		</header>
+		<!-- end of header -->
+
+		<!-- main -->
+		<div class="main">
+			<span class="shadow-top"></span>
+			<!-- shell -->
+			<div class="shell">
+				<div class="container">
+					<!-- testimonial -->
+					<section class="testimonial">
+						<p>
+							<strong>&#8220;</strong>
+							No one is immortal, in this world and neither is wealth, Therefore one should contribute 
+							according to one's capability for the welfare of the community and the nation. &nbsp;
+							<strong>&#8221;</strong>
+						</p>
+					</section>
+					<!-- testimonial -->
+
+					<section class="blog">
+						<!-- content -->
+						<div class="content">
+							<div class="cnt">
+								<h3>Our brief introduction</h3>
+								<p>
+									Inspired by the love for education of our respected parents Mr. Amrit Man Shrestha 
+									and Mrs. Prithvi Kumari Shrestha, we have established a non-profit social organization 
+									named 'Amrit-Prithvi Trust' with a view to contribute to the nation and people in 
+									different fields like education, health, arts, culture, social work and religion. 
+								</p>
+								<br/>
+								<p>
+									This Trust was registered with the District Administrative Office, Kathmandu, Nepal on 
+									3rd July, 1997 (19th Bhadra, 2054 B.S.) under the institute of Organization 
+									Registration Act 2034. 
+								</p>
+							</div>
+						</div>
+						<!-- end of content -->
+
+						<!-- sidebar -->
+						<aside class="sidebar">
+							<!-- widget -->
+							<div class="widget">
+								<h3>Executive Members</h3>
+								<ul>
+									<li class="last">
+										Prof. Dr. Pushpa Shrestha
+										<span>Chairperson</span>
+									</li>
+									<li class="last">
+										Mr. Prakash Man Shrestha
+										<span>Secretary</span>
+									</li>
+									<li class="last">
+										Mr. Vikash Man Shrestha
+										<span>Treasurer</span>
+									</li>
+									<li class="last">
+										Mrs. Jayenti Shrestha
+										<span>Assistant Secretary</span>
+									</li>
+									<li class="last">
+										Mrs. Prami Shrestha
+										<span>Assistant Treasurer</span>
+									</li>
+									<li class="last">
+										Mrs. Mala Rajbhandari Shrestha
+										<span>Member</span>
+									</li>
+									<li class="last">
+										Mr. Pranij Man Shrestha
+										<span>Member</span>
+									</li>
+								</ul>
+								<div class="cl">&nbsp;</div>
+							</div>
+							<!-- end of widget -->
+						</aside>
+						<!-- end of sidebar -->
+						<div class="cl">&nbsp;</div>
+					</section>	
+				</div>
+			<!-- end of shell -->
+			</div>
+			<!-- end of container -->
+		</div>
+		<!-- end of main -->
+
+	</div>	
+		
+	<!-- footer-push -->
+	<div id="footer-push"></div>
+	<!-- end of footer-push -->
+	
+	<!-- end of wrapper -->
+	<!-- footer -->
+	<div  id="footer">
+		<span class="shadow-bottom"></span>
+		<div class="footer-bottom">
+			<div class="shell">
+				<nav class="footer-nav">
+					<ul>
+						<li><a href="#">Home</a></li>
+						<li><a href="#">About</a></li>
+						<li><a href="#">Services</a></li>
+						<li><a href="#">Projects</a></li>
+						<li><a href="#">Solutions</a></li>
+						<li><a href="#">Jobs</a></li>
+						<li><a href="#">Blog</a></li>
+						<li><a href="#">Contacts</a></li>
+					</ul>
+					<div class="cl">&nbsp;</div>
+				</nav>
+				<p class="copy">
+					&copy; Copyright 2012<span>|</span> Design by 
+					<a href="http://chocotemplates.com" target"_blank">ChocoTemplates.com</a>
+				</p>
+			</div>
+		</div>
+	</div>
+	<!-- end of footer -->
+
+</body>
+</html>
