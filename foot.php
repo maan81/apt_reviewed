21ad2ae9b34d8e5235dbb2bec99df0744aad8e24
maan81 <pranijman@gmail.com>
2014-05-06 16:47:31 +0545

    footer

--- foot.php
+++ foot.php
@@ -3,21 +3,8 @@
 		<span class="shadow-bottom"></span>
 		<div class="footer-bottom">
 			<div class="shell">
-				<nav class="footer-nav">
-					<ul>
-						<li><a href="#">Home</a></li>
-						<li><a href="#">About</a></li>
-						<li><a href="#">Services</a></li>
-						<li><a href="#">Projects</a></li>
-						<li><a href="#">Solutions</a></li>
-						<li><a href="#">Jobs</a></li>
-						<li><a href="#">Blog</a></li>
-						<li><a href="#">Contacts</a></li>
-					</ul>
-					<div class="cl">&nbsp;</div>
-				</nav>
 				<p class="copy">
-					&copy; Copyright 2014<span>|</span> Design by 
+					Amrit-Prithvi Trust<sup>&copy;</sup> 2014<span>|</span> Design by 
 					<a href="http://chocotemplates.com" target"_blank">ChocoTemplates.com</a>
 				</p>
 			</div>
