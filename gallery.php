7834a6ce905284ba3ce78498ba0467b2957509a8
maan81 <pranijman@gmail.com>
2014-05-07 22:46:44 +0545

    photo albums

---
+++ gallery.php
@@ -0,0 +1,96 @@
+<?php $active = 'gallery' ?>
+<?php include('head.php') ?>		
+
+		<!-- top-nav -->
+		<?php include('top_nav.php') ?>
+		<!-- end of top-nav -->
+
+		<!-- header -->
+		<header  id="header">
+		</header>
+		<!-- end of header -->
+
+		<!-- main -->
+		<div class="main">
+			<span class="shadow-top"></span>
+			<!-- shell -->
+			<div class="shell">
+				<div class="container">
+
+					<section class="blog">
+						<!-- content -->
+						<div class="content">
+							<div class="cnt">
+							
+								<h3>Photo Albums</h3>
+								
+								<div>
+									<a href="">
+										<img src="images/000059_tgkoy_9.jpg">
+										<span>Event !</span>
+									</a>
+									<a href="">
+										<img src="images/000059_tgkoy_9.jpg">
+										<span>Event 2</span>
+									</a>
+									<a href="">
+										<img src="images/000059_tgkoy_9.jpg">
+										<span>Event 3</span>
+									</a>
+									<a href="">
+										<img src="images/000059_tgkoy_9.jpg">
+										<span>Event 4</span>
+									</a>
+									<a href="">
+										<img src="images/000059_tgkoy_9.jpg">
+										<span>Event 5</span>
+									</a>
+									<a href="">
+										<img src="images/000059_tgkoy_9.jpg">
+										<span>Event 6</span>
+									</a>
+									<a href="">
+										<img src="images/000059_tgkoy_9.jpg">
+										<span>Event 7</span>
+									</a>
+									<a href="">
+										<img src="images/000059_tgkoy_9.jpg">
+										<span>Event 8</span>
+									</a>
+									<a href="">
+										<img src="images/000059_tgkoy_9.jpg">
+										<span>Event 9</span>
+									</a>
+								</div>
+
+								<span class="paginate">
+								    <span class="current" style="">1</span>
+								    <a href="">2</a>
+								    <a href="">3</a>
+								    <a href="">4</a>
+								</span>
+
+							</div>
+
+						</div>
+						<!-- end of content -->
+
+						<!-- sidebar --><?php include('aside.php') ?><!-- end of sidebar -->
+						<div class="cl">&nbsp;</div>
+					</section>	
+				</div>
+			<!-- end of shell -->
+			</div>
+			<!-- end of container -->
+		</div>
+		<!-- end of main -->
+
+	</div>	
+		
+	<!-- footer-push -->
+	<div id="footer-push"></div>
+	<!-- end of footer-push -->
+	
+	<!-- end of wrapper -->
+
+<?php include('foot.php') ?>
