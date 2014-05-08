7d02ad94852ba784fedd8924ba19ed6df7b245a3
maan81 <pranijman@gmail.com>
2014-05-08 14:46:22 +0545

    preent resubmitting of contact

--- contact_submit.php
+++ contact_submit.php
@@ -46,10 +46,10 @@
 		                            $_POST["recaptcha_response_field"]);
 
 		if (!$resp->is_valid) {
+			$message=false;
 			$error_msg .= "<br/> reCAPTCHA error : ".$resp->error;
 
 		}else if(!$error_msg){
-
 			$to .= 'info@apt.org.np';
 
 			// subject
@@ -77,7 +77,7 @@
 								</table>
 							</body>
 						</html>';
-// echo $message;
+
 			// To send HTML mail, the Content-type header must be set
 			$headers  = 'MIME-Version: 1.0' . "\r\n";
 			$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
